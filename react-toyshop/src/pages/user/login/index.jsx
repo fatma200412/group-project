b8ffbd5d97ba0c238ea42b03968f12485ced43c7
sa3ina <sravnennaya@mail.ru>
2023-12-03 01:47:19 +0400

    lil bit of footer added

--- react-toyshop/src/pages/user/login/index.jsx
+++ react-toyshop/src/pages/user/login/index.jsx
@@ -27,69 +27,112 @@ function Login() {
   }, [dispatch]);
   console.log(cardProd);
   return (
-    <Container>
-      <Grid
-        container
-        spacing={8}
-        justifyContent="center"
-        alignItems="center"
-        style={{ minHeight: "100vh" }}
-      >
-        <Grid item xs={6}>
-          <Typography
-            style={{ fontFamily: "Mali", fontSize: "45px", fontWeight: "600" }}
-            variant="h3"
-            align="center"
-            color="RGB(31, 51, 50)"
-          >
-            Login
-          </Typography>
-          <div
-            style={{
-              gap: "17px",
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <Typography style={{ fontFamily: "Mali" }} variant="subtitle1">
-              Home
-            </Typography>
-            <Typography style={{ marginTop: "3px" }}>
-              <FontAwesomeIcon icon={faChevronRight} />
-            </Typography>
+    <>
+      <Container>
+        <Grid
+          container
+          spacing={8}
+          justifyContent="center"
+          alignItems="center"
+          style={{ minHeight: "100vh" }}
+        >
+          <Grid item xs={6}>
             <Typography
-              variant="subtitle1"
-              style={{ fontFamily: "Mali", marginBottom: "10px" }}
+              style={{
+                fontFamily: "Mali",
+                fontSize: "45px",
+                fontWeight: "600",
+              }}
+              variant="h3"
+              align="center"
+              color="RGB(31, 51, 50)"
             >
-              Account
+              Login
             </Typography>
-          </div>{" "}
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              gap: "20px",
-            }}
-          >
-            <label htmlFor="">Your email</label>
-            <TextField variant="outlined" />
-            <label htmlFor="">Password</label>
-            <TextField variant="outlined" />
-            <p>Forgot your password?</p>
-            <button className={styles.signin}> Sign in</button>
+            <div
+              style={{
+                gap: "17px",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Typography style={{ fontFamily: "Mali" }} variant="subtitle1">
+                Home
+              </Typography>
+              <Typography style={{ marginTop: "3px" }}>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                style={{ fontFamily: "Mali", marginBottom: "10px" }}
+              >
+                Account
+              </Typography>
+            </div>{" "}
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                gap: "20px",
+              }}
+            >
+              <label htmlFor="">Your email</label>
+              <TextField variant="outlined" />
+              <label htmlFor="">Password</label>
+              <TextField variant="outlined" />
+              <p>Forgot your password?</p>
+              <button className={styles.signin}> Sign in</button>
 
-            <div className={styles.textbetweenlines}>
-              <div className={styles.line}></div>
-              <p className={styles.or}>OR</p>
-              <div className={styles.line}></div>
+              <div className={styles.textbetweenlines}>
+                <div className={styles.line}></div>
+                <p className={styles.or}>OR</p>
+                <div className={styles.line}></div>
+              </div>
+
+              <button className={styles.create}>Create account</button>
             </div>
-
-            <button className={styles.create}>Create account</button>
-          </div>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <img
+        src="https://hongotheme.myshopify.com/cdn/shop/files/demo-kids-toy-bg-img-03.jpg?v=1698815628"
+        alt=""
+      />
+      <footer
+        className={styles.footer}
+        style={{ marginBottom: "300px", backgroundColor: "#D8E9E8" }}
+      >
+        <Container>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} md={3}>
+              <div className={styles.column}>
+                <Typography variant="h6">Column 1</Typography>
+                <Typography variant="body2">Content for column 1</Typography>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <div className={styles.column}>
+                <Typography variant="h6">Column 2</Typography>
+                <Typography variant="body2">Content for column 2</Typography>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <div className={styles.column}>
+                <Typography variant="h6">Column 3</Typography>
+                <Typography variant="body2">Content for column 3</Typography>
+              </div>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <div className={styles.column}>
+                <Typography variant="h6">Column 4</Typography>
+                <Typography variant="body2">Content for column 4</Typography>
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
+      </footer>
+    </>
   );
 }
 
