bc83ede7bf7da12175296cd653c0bfac86a17bf6
fatma200412 <bd7GQ9082@code.edu.az>
2023-12-02 13:36:38 +0400

    navbar

---
+++ react-toyshop/src/pages/user/userRoot/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../../layout/navbar";
+
+function UserRoot() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default UserRoot;
