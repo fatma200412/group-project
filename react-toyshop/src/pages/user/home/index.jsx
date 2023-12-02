a15e8641533f14ce0d54ff2d339be9fc4af004ff
fatma200412 <bd7GQ9082@code.edu.az>
2023-12-02 15:01:59 +0400

    navbar2

--- react-toyshop/src/pages/user/home/index.jsx
+++ react-toyshop/src/pages/user/home/index.jsx
@@ -2,11 +2,62 @@ import React from "react";
 import CardsProduct from "../../../components/cards";
 import style from "./index.module.css";
 
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+
 function Home() {
   return (
     <>
-      <h1 className={style.home}>home</h1>
-      <CardsProduct />
+      <div style={{ backgroundColor: "#88A9A8" }}>
+        <Container maxWidth="xl">
+          <Box
+            sx={{
+              flexGrow: 1,
+            }}
+          >
+            <Grid
+              container
+              spacing={0}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                // padding: "18px",
+                color: "#F7FCF7",
+                fontWeight: "500",
+                fontFamily: "Bradley hand,cursive",
+                textAlign: "center",
+              }}
+            >
+              <Grid
+                item
+                sm={12}
+                md={12}
+                lg={3}
+                xl={3}
+                style={{ borderRight: "1px solid #F7FCF7", padding: "20px" }}
+              >
+                <p>Refer a Friend. Get 20% Off</p>
+              </Grid>
+              <Grid
+                item
+                sm={12}
+                md={12}
+                lg={3}
+                xl={3}
+                style={{ borderRight: "1px solid #F7FCF7", padding: "20px" }}
+              >
+                <p>Subscribe Today and Get $10</p>
+              </Grid>
+              <Grid item sm={12} md={12} lg={3} xl={3}>
+                <p>Subscribe Today and Get $10</p>
+              </Grid>
+            </Grid>
+          </Box>
+        </Container>
+      </div>
     </>
   );
 }
--- react-toyshop/src/pages/user/products/index.jsx
+++ react-toyshop/src/pages/user/products/index.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Products() {
-  return <div>Products</div>;
+  return (
+    <>
+      <h1>Products</h1>
+    </>
+  );
 }
 
 export default Products;
